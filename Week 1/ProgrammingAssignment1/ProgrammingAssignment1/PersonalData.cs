0417c0508a452cd3a3762b961fa82e76c6e47fec
Nguyen Minh Duc <86579678+ducnguyenedu@users.noreply.github.com>
2021-12-16 18:53:05 +0700

    commit

---
+++ Week 1/ProgrammingAssignment1/ProgrammingAssignment1/PersonalData.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment1
+{
+    /// <summary>
+    /// A class holding personal data for a person
+    /// </summary>
+    public class PersonalData
+    {
+        #region Fields
+
+        // declare your fields here
+        string city;
+        string country;
+        string firstName;
+        string lastName;
+        string middleName;
+        string phoneNumber;
+        string postalCode;
+        string state;
+        string streetAddress;
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the person's first name
+        /// </summary>
+        public string FirstName
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return firstName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's middle name
+        /// </summary>
+        public string MiddleName
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return middleName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's last name
+        /// </summary>
+        public string LastName
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return lastName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's street address
+        /// </summary>
+        public string StreetAddress
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return streetAddress;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's city or town
+        /// </summary>
+        public string City
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return city;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's state or province
+        /// </summary>
+        public string State
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return state;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's postal code
+        /// </summary>
+        public string PostalCode
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return postalCode;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's country
+        /// </summary>
+        public string Country
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return country;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's phone number (digits only, no 
+        /// parentheses, spaces, or dashes)
+        /// </summary>
+        public string PhoneNumber
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return phoneNumber;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// Reads personal data from a file. If the file
+        /// read fails, the object contains an empty string for all
+        /// the personal data
+        /// </summary>
+        /// <param name="fileName">name of file holding personal data</param>
+        public PersonalData(string fileName)
+        {
+            // your code can assume we know the order in which the
+            // values appear in the string; it's the same order as
+            // they're listed for the properties above. We could do 
+            // something more complicated with the names and values, 
+            // but that's not necessary here
+            if (File.Exists(fileName))
+            {
+                string text = System.IO.File.ReadAllText(fileName);
+                List<string> list = new List<string>();
+
+                var values = text.Split(',');
+                foreach (var item in values)
+                {
+                    list.Add(item);
+                }
+
+                firstName = list[0];
+                middleName = list[1];
+                lastName = list[2];
+                streetAddress = list[3];
+                city = list[4];
+                state = list[5];
+                postalCode = list[6];
+                country = list[7];
+                phoneNumber = list[8];
+            }
+            // IMPORTANT: The mono compiler the automated grader uses
+            // does NOT support the string Split method. You have to 
+            // use the IndexOf method to find comma locations and the
+            // Substring method to chop off the front of the string
+            // after you extract each value to extract and save the
+            // personal data
+
+
+
+
+        }
+
+        #endregion
+    }
+}
